60d66e5e366c3e6efbe63d41f8873d9956ae354b
Nelly <nelly@bitwala.com>
2021-10-19 11:30:55 +0200

    update

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,23 +1,47 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
+import GraphemeSplitter from "grapheme-splitter";
 
 import img_profile from "assets/images/nelly_moon.png";
 
 function AboutSection() {
+  function stringSplitter(string) {
+    const splitter = new GraphemeSplitter();
+    return splitter.splitGraphemes(string);
+  }
+  const typewriter = ["junior web developer", "musician", "artist", "creative person :)"];
   return (
     <div className="about">
       <div className="about-top">
         <div className="about-info">
-          <h2>Hey,<br />I'm <span className="nelly">Nelly</span></h2>
-          <p>
-            a junior<span className="web-developer" > web developer</span>, <span className="musician">musician</span>,<br/><span className="artist">artist</span> and all around <span className="creative-person">creative
-            person</span>
-          </p>
+          <h2>
+            Hey,
+            <br />
+            I'm <span className="nelly">Nelly</span>
+          </h2>
+                  {/* <p>
+                      
+            </p> */}
+                           <Typewriter
+            //   css={typewriter}
+            options={{
+              strings: typewriter,
+              autoStart: true,
+              loop: true,
+              delay: 55,
+              stringSplitter,
+            }}
+          />
+                      {/* junior<span className="web-developer"> web developer</span>,{" "}
+            <span className="musician">musician</span>,<br />
+            <span className="artist">artist</span> and all around{" "}
+            <span className="creative-person">creative person</span> */}
         </div>
         <div className="about-photo">
           <img src={img_profile} alt="creepy digital art profile" />
         </div>
       </div>
-          {/* <div className="about-bottom">
+      {/* <div className="about-bottom">
               <SocialMediaSection/>
       </div> */}
     </div>
--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,18 +1,18 @@
 export const ProjectData = [
   {
     id: 1,
-    title: "Digital Art",
+    title: "Digital Artist",
     about:
-      "I've been making art for as long as I can remember, mostly with traditional mediums like watercolour or liners. About a year ago I tried drawing digitally for the first time and fell in love with the endless the possibilities of it. Shortly after I decided to start selling some of my designs on Redbubble. Below are the link to my shop and to some of my (more recent) digital artworks :)",
+      "I've been making art for as long as I can remember, mostly with traditional mediums like watercolour or liners. About a year ago I tried drawing digitally for the first time and fell in love with the endless the possibilities of it.",
     button:
       "https://drive.google.com/drive/folders/1Oa1DzKGkJ7K1VdQROjF66Q7e7kCN1CSf",
-        image: require("../../assets/images/bag.png"),
-    shopLink: "https://www.redbubble.com/people/schoengeister/shop?asc=u&ref=account-nav-dropdown"
+    image: require("../../assets/images/creepy_face.png"),
   },
   {
     id: 1,
-    title: "Current work",
-    about: "Nuri is the app to manage, save and grow your money. Invest in cryptocurrencies, create savings plans & earn up to 5% interest per year on bitcoin directly from a German bank account.",
+    title: "Fullstack Web Developer",
+    about:
+      "Nuri is the app to manage, save and grow your money. Invest in cryptocurrencies, create savings plans & earn up to 5% interest per year on bitcoin directly from a German bank account.",
     tags: [
       "ReactJS",
       "JavaScript",
@@ -24,5 +24,15 @@ export const ProjectData = [
     ],
     demo: "https://nuri.com/",
     image: require("../../assets/images/nuri.png"),
+  },
+  {
+    id: 3,
+    title: "Product Designer",
+    about:
+      "Shortly after I tried digital art for the first time I started selling some of my designs on Redbubble. Link to the shop is below!",
+
+    image: require("../../assets/images/bag.png"),
+    shopLink:
+      "https://www.redbubble.com/people/schoengeister/shop?asc=u&ref=account-nav-dropdown",
   },
 ];
